c1412a2edc58a04da70ce343f62e3e6758ba3713
Dengerous <99312935+dengerous53@users.noreply.github.com>
2023-03-22 22:30:13 +0530

    Update __main__.py

--- Adarsh/__main__.py
+++ Adarsh/__main__.py
@@ -66,7 +66,7 @@ async def start_services():
     print('\n')
     print('---------------------------------------------------------------------------------------------------------')
     print('---------------------------------------------------------------------------------------------------------')
-    print(' follow me for more such exciting bots! https://github.com/adarsh-goel')
+    print(' follow me for more such exciting bots! https://github.com/dengerous53')
     print('---------------------------------------------------------------------------------------------------------')
     print('\n')
     print('----------------------- Service Started -----------------------------------------------------------------')
@@ -76,7 +76,7 @@ async def start_services():
     if Var.ON_HEROKU:
         print('                        app runnng on =>> {}'.format(Var.FQDN))
     print('---------------------------------------------------------------------------------------------------------')
-    print('Give a star to my repo https://github.com/adarsh-goel/filestreambot-pro  also follow me for new bots')
+    print('Special thanks to tg : @nexus_shubhu to provide his in player codes')
     print('---------------------------------------------------------------------------------------------------------')
     await idle()
 
